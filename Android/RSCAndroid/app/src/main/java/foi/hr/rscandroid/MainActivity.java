97adfb786f089ac18e33c51d6e9465e3603e0056
skliba <stefano.kliba@infinum.hr>
2016-11-23 17:19:43 +0100

    Add Butterknife, Retrofit & JodaTime and make a .gitignore file

---
+++ Android/RSCAndroid/app/src/main/java/foi/hr/rscandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package foi.hr.rscandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
