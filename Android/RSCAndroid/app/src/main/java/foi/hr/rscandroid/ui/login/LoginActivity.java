5f7f076a4f9d32b820b970f67c372dc6e9a0facf
skliba <stefano.kliba@infinum.hr>
2016-11-24 23:22:52 +0100

    Create login layout

---
+++ Android/RSCAndroid/app/src/main/java/foi/hr/rscandroid/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package foi.hr.rscandroid.ui.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import foi.hr.rscandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
