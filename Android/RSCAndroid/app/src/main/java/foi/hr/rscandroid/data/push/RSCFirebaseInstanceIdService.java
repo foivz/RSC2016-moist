dcda14c3ab2bc3ba7e1b15a370f6dc3782281758
skliba <stefano.kliba@infinum.hr>
2016-11-24 22:29:14 +0100

    Store Firebase token to SP

--- Android/RSCAndroid/app/src/main/java/foi/hr/rscandroid/data/push/RSCFirebaseInstanceIdService.java
+++ Android/RSCAndroid/app/src/main/java/foi/hr/rscandroid/data/push/RSCFirebaseInstanceIdService.java
@@ -3,7 +3,11 @@ package foi.hr.rscandroid.data.push;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import foi.hr.rscandroid.ui.shared.SharedPrefsHelper;
+
 public class RSCFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    public static final String FIREBASE_TOKEN = "FirebaseToken";
 
     public RSCFirebaseInstanceIdService() {
     }
@@ -12,11 +16,11 @@ public class RSCFirebaseInstanceIdService extends FirebaseInstanceIdService {
     public void onTokenRefresh() {
         super.onTokenRefresh();
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-
         sendRegistrationToServer(refreshedToken);
     }
 
     private void sendRegistrationToServer(String refreshedToken) {
-
+        SharedPrefsHelper.clearSingleSharedPrefsItem(FIREBASE_TOKEN);
+        SharedPrefsHelper.setSharedPrefsString(FIREBASE_TOKEN, refreshedToken);
     }
 }
