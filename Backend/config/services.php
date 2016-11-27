cf7322bfd96dd0100780a0d3515766ea5074f177
mjurinic <jurinic.mislav@gmail.com>
2016-11-27 08:19:27 +0100

    minor fixes

--- Backend/config/services.php
+++ Backend/config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'we-dont-need-this-atm',
+        'redirect' => 'http://138.65.85.154:8000/social/facebook/redirect',
     ],
 
     'google' => [
